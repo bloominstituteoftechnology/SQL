e99c4e8b7f0c3c8a5b513fd14f46fcef8709558d
Christopher Beards <beardscm@gmail.com>
2018-06-06 17:31:10 -0400

    Day One; Complete

--- setup.sql
+++ setup.sql
@@ -78,3 +78,35 @@ SELECT * FROM album;
 SELECT * FROM album WHERE release_year >= 1975 AND release_year < 1990;
 SELECT * FROM album WHERE title LIKE "Super D%";
 SELECT * FROM album WHERE release_year IS NULL;
+
+
+-- Show all track titles from Super Funky Album.
+SELECT track.title FROM track, album
+    WHERE track.album_id = album.id AND album.title = "Super Funky Album";
+
+-- Same query as above, but rename the column from title to Track_Title in the output.
+SELECT track.title AS "Track_Title" FROM track, album
+    WHERE track.album_id = album.id AND album.title = "Super Funky Album";
+
+-- Select all album titles by Han Solo.
+SELECT album.title FROM album, artist_album, artist
+    WHERE artist_album.album_id = album.id AND
+    artist_album.artist_id = artist.id AND
+    artist.name = "Han Solo";
+
+-- Select the average year all albums were released.
+SELECT AVG(release_year) FROM album;
+
+-- Select the average year all albums by Leia and the Ewoks were released.
+SELECT AVG(release_year) FROM album, artist_album, artist
+    WHERE artist_album.album_id = album.id AND
+    artist_album.artist_id = artist.id AND
+    artist.name = "Leia and the Ewoks";
+
+-- Select the number of artists.
+SELECT COUNT(*) FROM artist;
+
+-- Select the number of tracks on Super Dubstep Album.
+SELECT COUNT(*) FROM track, album
+    WHERE track.album_id = album.id AND
+    album.title = "Super Dubstep Album";
