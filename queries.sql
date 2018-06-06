9ae44e132f6cefdbcd9023128f4c1b9c6b101058
Mephestys <mephestys@gmail.com>
2018-06-06 15:18:17 -0600

    Completed day one exercises

--- queries.sql
+++ queries.sql
@@ -17,3 +17,32 @@ SELECT * FROM album WHERE title LIKE "Super D%";
 .print "Show all albums that have no release year:"
 SELECT * FROM album WHERE release_year IS NULL;
 .print ""
+
+.print "Show all track titles from Super Funky Album:"
+SELECT title FROM track WHERE album_id = 2;
+.print ""
+
+.print "Show all track titles from Super Funky Album with Track_Title as column name:"
+SELECT title AS Track_Title FROM track WHERE album_id = 2;
+.print ""
+
+.print "All album titles by Han Solo:"
+SELECT title FROM album, artist_album, artist
+  WHERE album_id = album.id AND artist_id = artist.id AND artist_id = 3;
+.print ""
+
+.print "Average year all albums released:"
+SELECT AVG(release_year) AS "average" FROM album;
+.print ""
+
+.print "Average year all albums by Leia and the Ewoks released:"
+SELECT AVG(release_year) AS "average" FROM album, artist_album, artist
+  WHERE album_id = album.id AND artist_id = artist.id AND artist_id = 2;
+.print ""
+
+.print "Number of artists:"
+SELECT COUNT(*) AS "count" FROM artist;
+.print ""
+
+.print "Number of tracks on Super Dubstep Album:"
+SELECT COUNT(*) AS "count" FROM track, album WHERE album_id = album.id AND album.id = 5;
