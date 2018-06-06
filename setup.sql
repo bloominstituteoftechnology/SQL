2d38edd1b1ad7ff71e2b8989eb85f32e7c1f1368
Christopher Beards <beardscm@gmail.com>
2018-06-06 16:51:33 -0400

    Added Tables and Drop Tables

--- setup.sql
+++ setup.sql
@@ -1,3 +1,33 @@
+DROP TABLE IF EXISTS artist_album;
+DROP TABLE IF EXISTS album;
+DROP TABLE IF EXISTS artist;
+DROP TABLE IF EXISTS track;
+
+CREATE TABLE album (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title VARCHAR(128) NOT NULL,
+    release_year INTEGER
+);
+
+CREATE TABLE artist (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(128) NOT NULL
+);
+
+CREATE TABLE track (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title VARCHAR(128),
+    album_id INTEGER,
+    FOREIGN KEY(album_id) REFERENCES album(id)
+);
+
+CREATE TABLE artist_album (
+    artist_id INTEGER,
+    album_id INTEGER,
+    FOREIGN KEY(artist_id) REFERENCES artist(id),
+    FOREIGN KEY(album_id) REFERENCES album(id)
+);
+
 INSERT INTO album (title, release_year) VALUES ("Super Awesome Album", 1990);
 INSERT INTO album (title) VALUES ("Super Funky Album");
 INSERT INTO album (title, release_year) VALUES ("Super Disco Album", 1978);
