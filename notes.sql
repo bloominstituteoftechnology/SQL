c877cbacab8dea88c7dfee013abee04415d6b924
Mephestys <mephestys@gmail.com>
2018-06-07 17:48:01 -0600

    Day two exercise completed

--- notes.sql
+++ notes.sql
@@ -10,7 +10,7 @@ DROP TABLE IF EXISTS author;
 -- What are the columns that a note table needs?
 CREATE TABLE note (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  author_id INTEGER REFERENCES author(id) NOT NULL, -- A note should have a foreign key pointing to an author in an author table.
+  author_id INTEGER REFERENCES author(id) ON DELETE CASCADE NOT NULL, -- A note should have a foreign key pointing to an author in an author table.
   title VARCHAR(128) NOT NULL,
   content VARCHAR(1024) NOT NULL,
   created_on DATETIME NOT NULL
@@ -29,7 +29,38 @@ CREATE TABLE author (
 -- Insert authors to the author table.
 INSERT INTO author (name) VALUES ("Lando Calrissian");
 INSERT INTO author (name) VALUES ("Yoda");
+INSERT INTO author (name) VALUES ("Mon Mothma");
 
 -- Insert notes to the note table.
 INSERT INTO note (author_id, title, content, created_on)
-  VALUES (1, "Note One", "Betray Han Solo.", DATETIME('now'));
+  VALUES (1, "Lando Note One", "Betray Han Solo.", DATETIME('now'));
+INSERT INTO note (author_id, title, content, created_on)
+  VALUES (1, "Lando Note Two", "Betray the Empire.", DATETIME('now'));
+INSERT INTO note (author_id, title, content, created_on)
+  VALUES (2, "Yoda Note One", "Live on a swamp planet for decades.", DATETIME('now'));
+INSERT INTO note (author_id, title, content, created_on)
+  VALUES (2, "Yoda Note Two", "Be pointlessly cryptic when Luke finds me.", DATETIME('now'));
+INSERT INTO note (author_id, title, content, created_on)
+  VALUES (2, "Yoda Note Three", "Tell Luke he fails, then keep training him anyway.", DATETIME('now'));
+INSERT INTO note (author_id, title, content, created_on)
+  VALUES (3, "Mon Note One", "Send Bothans to their deaths.", DATETIME('now'));
+
+.print "Select all notes by an author's name. (Lando Calrissian)"
+SELECT title, content FROM note, author
+  WHERE note.author_id = author.id AND
+  author.name = "Lando Calrissian";
+.print ""
+
+.print "Select author for a particular note by note ID. (3, should be Yoda)"
+SELECT name FROM note, author
+  WHERE note.author_id = author.id AND note.id = 3;
+.print ""
+
+.print "Select the names of all the authors along with the number of notes they each have."
+SELECT name, COUNT(*) AS "count" FROM note, author
+  WHERE note.author_id = author.id GROUP BY author_id;
+.print ""
+
+-- Delete authors from the author table.
+DELETE FROM author WHERE name = "Lando Calrissian";
+-- ON DELETE CASCADE is set on the author_id column in the note table
