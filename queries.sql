d2f375edf81031ef87ef9ec4272f95f71273cd22
Dixie Korley <dixiekorley@gmail.com>
2018-06-07 13:11:11 -0400

    Showing albums exercises

--- queries.sql
+++ queries.sql
@@ -26,3 +26,17 @@ CREATE TABLE artist_album (
     artist_id INTEGER REFERENCES artist(id),
     album_id INTEGER REFERENCES album(id)
 );
+
+-- Show all albums
+SELECT * FROM album
+
+-- Show all albums made between 1975 and 1990.
+SELECT * FROM album WHERE release_year >= 1975 AND release_year <= 1990;
+
+--Show all albums whose name start with Super D
+SELECT * FROM album WHERE title LIKE 'Super D%';
+
+-- Show all albums that have no release year.
+SELECT * FROM album WHERE release_year IS NULL;
+
+
