8ab35d80a1813c0f80c029ad53e2f38ee74c0f34
Dixie Korley <dixiekorley@gmail.com>
2018-06-07 19:07:21 -0400

    Currently working on Day 2

--- notes.sql
+++ notes.sql
@@ -8,5 +8,37 @@ CREATE TABLE note (
     title VARCHAR(128) NOT NULL,
     content VARCHAR(1024),
     author_id INTEGER REFERENCES author(id),
-    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+  -- created_at TIMESTAMP NOT NULL DEFAULT(DATETIME()),
+   -- last_modified TIMESTAMP NOT NULL DEFAULT(DATETIME()),
+    -- created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE author (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(128) NOT NULL
+);
+
+INSERT INTO author (name) VALUES ('Dixie');
+INSERT INTO author (name) VALUES ('Edmund');
+
+INSERT INTO note (title, content, author_id) VALUES ('First Note', 'Yay, first note from Dixie', 1);
+INSERT INTO note (title, content, author_id) VALUES ('First Note', 'Yay, first note from Edmun', 2);
+INSERT INTO note (title, content, author_id) VALUES ('Second Note', 'Yay, second note from Dixie', 1);
+INSERT INTO note (title, content, author_id) VALUES ('Second Note', 'Yay, second note from Edmun', 2);
+INSERT INTO note (title, content, author_id) VALUES ('Third Note', 'Yay, third note from Dixie', 1);
+INSERT INTO note (title, content, author_id) VALUES ('Third Note', 'Yay, third note from Edmun', 2);
+
+SELECT * FROM note, author
+    WHERE author.name = 'Dixie'
+    AND note.author_id = author.id;
+
+SELECT * FROM note, author
+    WHERE note.id = '2'
+    AND note.author_id = author.id;
+
+SELECT COUNT(*), author.name FROM note, author
+   WHERE note.author_id = author.id
+   GROUP BY author.name; 
+
+DELETE FROM note WHERE author_id IN (1);
+DELETE FROM author WHERE author.name = 'Dixie';
