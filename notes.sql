636e703e7aea5fb3d7252d76f1a872a0440725b1
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-06-07 23:09:58 -0400

    Wrote a few queries for notes.sql

--- notes.sql
+++ notes.sql
@@ -10,27 +10,6 @@
 
 /* * What columns are needed for the author table? */
 
-/* Write queries that: */
-
-/* * Insert authors to the author table. */
-
-/* * Insert notes to the note table. */
-
-/* * Select all notes by an author's name. */
-
-/* * Select author for a particular note by note ID. */
-
-/* * Select the names of all the authors along with the number of notes they each have. (Hint: `GROUP BY`.) */
-
-/* * Delete authors from the author table. */
-/*   > Note that SQLite doesn't enforce foreign key constrains by default. You have */
-/*   > to enable them by running `PRAGMA foreign_keys = ON;` before your queries. */
-  
-/*   * What happens when you try to delete an author with an existing note? */
-/*   * How can you prevent this? */
-
-/* Submit a file `notes.sql` with the queries that build (`CREATE TABLE`/`INSERT`) */
-/* and query the database as noted above. */
 
 CREATE TABLE note (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -50,19 +29,41 @@ CREATE TABLE note_author (
   author_id INTEGER REFERENCES author(id)
 );
 
+
+/* Write queries that: */
+
+/* * Insert authors to the author table. */
 INSERT INTO author (name) VALUES ('Nikhil');
 INSERT INTO author (name) VALUES ('Zephyr');
 INSERT INTO author (name) VALUES ('Pickle');
 
-INSERT INTO note (title, content, author_id) VALUES ("Test Note By Nikhil", "Nikhil's note content would be here.", 1);
-INSERT INTO note (title, content, author_id) VALUES ("Test Note By Zephyr", "Zephyr's note content would be here.", 2);
-INSERT INTO note (title, content, author_id) VALUES ("Test Note By Pickle", "Pickle's note content would be here.", 3);
+/* * Insert notes to the note table. */
+INSERT INTO note (title, content, author_id) VALUES ("Test Note 1 By Nikhil", "Nikhil's note 1 content would be here.", 1);
+INSERT INTO note (title, content, author_id) VALUES ("Test Note 1 By Zephyr", "Zephyr's note 1 content would be here.", 2);
+INSERT INTO note (title, content, author_id) VALUES ("Test Note 1 By Pickle", "Pickle's note 1 content would be here.", 3);
+INSERT INTO note (title, content, author_id) VALUES ("Test Note 2 By Nikhil", "Nikhil's note 2 content would be here.", 1);
+INSERT INTO note (title, content, author_id) VALUES ("Test Note 2 By Zephyr", "Zephyr's note 2 content would be here.", 2);
+INSERT INTO note (title, content, author_id) VALUES ("Test Note 2 By Pickle", "Pickle's note 2 content would be here.", 3);
 
 INSERT INTO note_author (note_id, author_id) VALUES (1, 1);
 INSERT INTO note_author (note_id, author_id) VALUES (2, 2);
 INSERT INTO note_author (note_id, author_id) VALUES (3, 3);
+INSERT INTO note_author (note_id, author_id) VALUES (4, 1);
+INSERT INTO note_author (note_id, author_id) VALUES (5, 2);
+INSERT INTO note_author (note_id, author_id) VALUES (6, 3);
 
-SELECT * FROM note;
-SELECT * FROM author;
-SELECT * FROM note_author;
 
+/* * Select all notes by an author's name. */
+SELECT * FROM note, author WHERE note.author_id = author.id AND author.id = 1;
+
+/* * Select author for a particular note by note ID. */
+SELECT author.name, author.id FROM author, note WHERE note.author_id = author.id AND note.id = 2;
+
+/* * Select the names of all the authors along with the number of notes they each have. (Hint: `GROUP BY`.) */
+
+/* * Delete authors from the author table. */
+/*   > Note that SQLite doesn't enforce foreign key constrains by default. You have */
+/*   > to enable them by running `PRAGMA foreign_keys = ON;` before your queries. */
+  
+/*   * What happens when you try to delete an author with an existing note? */
+/*   * How can you prevent this? */
