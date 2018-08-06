098ae1ed9372c464dbb4b0e56e90758fd4919a83
Mark Marchant <jtla3@yahoo.com>
2018-08-06 14:09:44 -0700

    created file to hold on my tables - tables.sql -

--- setup.sql
+++ setup.sql
@@ -1,3 +1,27 @@
+CREATE TABLE album (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title VARCHAR(128) NOT NULL,
+    release_year INTEGER
+);
+
+CREATE TABLE artist_album (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(128) NOT NULL
+);
+
+CREATE TABLE track (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title VARCHAR(128) NOT NULL,
+    album_id INTEGER REFERENCES album(id)
+);
+
+CREATE TABLE artist_album (
+    artist_id INTEGER REFERENCES artist(id),
+    album_id INTEGER REFERENCES album(id)
+);
+
+
+
 INSERT INTO album (title, release_year) VALUES ("Super Awesome Album", 1990);
 INSERT INTO album (title) VALUES ("Super Funky Album");
 INSERT INTO album (title, release_year) VALUES ("Super Disco Album", 1978);
