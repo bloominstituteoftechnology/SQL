492297bb68c7c61312e8a063a71482a806a36448
Henry K <mykim1891@hotmail.com>
2018-08-07 13:48:40 -0700

    Renamed files appropriately and created note.sql

---
+++ note.sql
@@ -0,0 +1,8 @@
+INSERT INTO note (title, content) VALUES ('Week 12: Front-end Dev.', 'There are various libraries you can utilize to build front-end of your website. Some includes bootstrap, React, etc.')
+INSERT INTO note (title, content) VALUES ('Random title','Random notes. Just scribling randomly.')
+INSERT INTO note (title, content) VALUES ('Another random title','Just another random writing to test the function of sqlite.')
+
+INSERT INTO author (name) VALUES ('Guy')
+INSERT INTO author (name) VALUES ('Anther Guy')
+INSERT INTO author (name) VALUES ('Very random guy')
+INSERT INTO author (name) VALUES ('Keanu Reaves')
