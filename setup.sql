c67123cc02604a0bb036819fc0984f83187aa1fc
Justin M <jekm321@gmail.com>
2018-08-07 13:42:31 -0400

    added build table methods

--- setup.sql
+++ setup.sql
@@ -1,3 +1,29 @@
+CREATE TABLE album (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title VARCHAR(128) NOT NULL,
+    release_year INTEGER
+);
+
+CREATE TABLE artist (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(128) NOT NULL
+);
+
+CREATE TABLE track (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title VARCHAR(128) NOT NULL,
+    album_id INTEGER,
+    FOREIGN KEY (album_id) REFERENCES album(id)
+);
+
+CREATE TABLE artist_album (
+  artist_album_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  artist_id INTEGER,
+  album_id INTEGER,
+  FOREIGN KEY (artist_id) REFERENCES artist(id),
+  FOREIGN KEY (album_id) REFERENCES ablum(id)
+);
+
 INSERT INTO album (title, release_year) VALUES ("Super Awesome Album", 1990);
 INSERT INTO album (title) VALUES ("Super Funky Album");
 INSERT INTO album (title, release_year) VALUES ("Super Disco Album", 1978);
