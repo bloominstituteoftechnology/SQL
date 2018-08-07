916f89a1e21eba719e32653fd84ec231f9fcc531
unknown <bsa6.23.94@gmail.com>
2018-08-07 11:10:02 -0400

    like not is on albumsql

--- album.sql
+++ album.sql
@@ -5,6 +5,6 @@ SELECT * FROM album;
 
 SELECT * FROM album where release_year > 1974 and release_year < 1991;
 
-SELECT * FROM album where title IS 'SUPER D';
+SELECT * FROM album where title LIKE 'SUPER D%';
 
 SELECT * FROM album where release_year IS NULL;
