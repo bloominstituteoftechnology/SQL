e2ecb4b3e0335e3892f1e88e32c9277835270443
unknown <bsa6.23.94@gmail.com>
2018-08-07 11:01:38 -0400

    new db because autoincrement

--- misc.sql
+++ misc.sql
@@ -1,6 +1,16 @@
+.mode column
+.header on
+
 SELECT title FROM track WHERE album_id IS (SELECT id FROM album WHERE title IS 'Super Funky Album');
 
 SELECT title AS Track_Title FROM track WHERE album_id IS (SELECT id FROM album WHERE title IS 'Super Funky Album');
 
-SELECT title FROM album WHERE album_id IS (SELECT id FROM artist_album WHERE artist_id IS (SELECT id FROM artist WHERE name IS 'Han Solo'));
+SELECT title FROM album WHERE id IS (SELECT album_id FROM artist_album WHERE artist_id IS (SELECT id FROM artist WHERE name IS 'Han Solo'));
 
+SELECT AVG(release_year) AS AVERAGE_YEAR_THING FROM album;
+
+SELECT AVG(release_year) AS AVERAGE_YEAR_THING FROM album WHERE id IS (SELECT album_id FROM artist_album WHERE artist_id IS (SELECT id FROM artist WHERE name IS 'Leia and the Ewoks'));
+
+SELECT COUNT(*) as Total_Artists FROM artist;
+
+SELECT COUNT(title) as Number_Of_Tracks FROM track WHERE album_id IS (SELECT id FROM album WHERE title IS 'Super Dubstep Album');
