a0048969831e01b8e8d409cbc46b60e01b07efbc
ajf412 <aaronjfaulkner@yahoo.com>
2018-08-08 03:25:58 -0400

    Day two WIP.  Still need last 2 objectives:  Display users and their note count, and delete authors.

--- day1.sql
+++ day1.sql
@@ -1,6 +1,6 @@
 SELECT * FROM album;
 SELECT * FROM album WHERE release_year > 1975 AND release_year < 1990;
-SELECT * FROM album WHERE title LIKE '%Super D%';
+SELECT * FROM album WHERE title LIKE 'Super D%';
 SELECT * FROM album WHERE release_year IS NULL;
 
 SELECT track.title FROM track WHERE track.album_id = 2;
