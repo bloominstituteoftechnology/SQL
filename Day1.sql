ac9d103d4894a9f6ba6a7684dc95b9cb1c9d1fae
Nedu Robert <nedumrobert@gmail.com>
2018-08-08 16:16:57 +0100

    Completed SQL queries

--- Day1.sql
+++ Day1.sql
@@ -4,6 +4,19 @@ CREATE TABLE track (id INT NOT NULL AUTOINCREMENT PRIMARY KEY, title VARCHAR(128
 
 CREATE TABLE artist_album (artist_id INT REFERENCES artist(id), album_id INT REFERENCES album(id));
 
-CREATE TABLE album (id INT NOT NULL AUTOINCREMENT PRIMARY KEY, title VARCHAR(128), release_year INT)
+CREATE TABLE album (id INT NOT NULL AUTOINCREMENT PRIMARY KEY, title VARCHAR(128), release_year INT);
 
-CREATE TABLE artist(id INT NOT NULL AUTOINCREMENT PRIMARY KEY, name VARCHAR(128) NOT NULL)
+CREATE TABLE artist(id INT NOT NULL AUTOINCREMENT PRIMARY KEY, name VARCHAR(128) NOT NULL);
+
+-- SELECT QUERIES --
+SELECT * FROM album;
+SELECT * FROM album WHERE release_year >= 1975 AND release_year <=1990;
+SELECT * FROM album WHERE title LIKE 'Super D%';
+SELECT * FROM album WHERE release_year IS NULL;
+SELECT track.title FROM track, album WHERE track.album_id = album.id AND album.title = "Super Funky Album";
+SELECT track.title AS "Track_Title" FROM track, album WHERE track.album_id = album.id AND album.title = "Super Funky Album";
+SELECT album.title FROM album, artist_album, artist WHERE artist_album.album_id = album.id AND artist_album.artist_id = artist.id AND artist.name = "Han Solo";
+SELECT AVG(release_year) FROM album;
+SELECT AVG(album.release_year) FROM album, artist_album, artist WHERE artist_album.album_id = album.id AND artist_album.artist_id = artist.id AND artist.name = "Leia and the Ewoks";
+SELECT COUNT(*) FROM artist;
+SELECT COUNT(*) FROM track, album WHERE track.album_id = album.id AND album.title = "Super Dubstep Album";
