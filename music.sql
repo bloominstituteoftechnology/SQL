c9160377b42213f7194d77ab6b028205c4badd27
kelly <kelfro@fastmail.com>
2018-09-03 11:55:37 -0700

    Finished Day 1 exercises

--- music.sql
+++ music.sql
@@ -50,10 +50,51 @@ id          title              release_year
 5           Super Dubstep Alb
 
 /*Show all track titles from Super Funky Album*/
+SELECT track.title FROM track, album WHERE track.album_id = album.id AND album.title = 'Super Funky Album';
+title
+-------------------
+Super Funky Track 1
+Super Funky Track 2
+Super Funky Track 3
+Super Funky Track 4
 
 /*Same as above but rename the column from title to Track_title in the output*/
+SELECT track.title AS Track_Title from track, album WHERE track.album_id=album.id AND album.title = 'Super Funky Album';
+Track_Title
+-------------------
+Super Funky Track 1
+Super Funky Track 2
+Super Funky Track 3
+Super Funky Track 4
+
 /*Select all album titles by Han Solo*/
+ SELECT album.title from album, artist, artist_album WHERE artist.id = artist_album.artist_id AND album.id = artist_album.album_id AND artist.name = 'Han Solo';
+title
+-----------------
+Super Disco Album
+Super Hairband Al
+
 /*Select the average year all albums were released*/
+SELECT AVG(release_year) from album
+AVG(release_year)
+-----------------
+1984.0
+
 /*Select the average year all albums by Leia and the Ewoks were released*/
+SELECT AVG(release_year) from album, artist, artist_album WHERE artist.id = artist_album.artist_id AND album.id = artist_album.album_id AND artist.name = 'Leia and the Ewoks';
+AVG(release_year)
+-----------------
+1990.0
+
 /*Select the number of artists*/
+SELECT COUNT(id) FROM artist;
+COUNT(id)
+----------
+3
+
 /*Select the number of tracks on Super Dubstep Album*/
+SELECT COUNT(track.id) FROM track, album WHERE album.id = track.album_id AND album.title = 'Super Dubstep Album';
+COUNT(track.id)
+---------------
+5
+
