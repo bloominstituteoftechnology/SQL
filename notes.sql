4f190fabcd5c2a3195da8a683d08fc5c0af89e3b
JSNavey <jsnavey@gmail.com>
2018-09-03 22:45:26 -0400

    Implement SQL SELECT. Finish Day 2

--- notes.sql
+++ notes.sql
@@ -34,6 +34,7 @@ What columns are needed for the author table?
 CREATE TABLE note (
 id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
 author_id INTEGER REFERENCES author(id),
+-- can write FOREIGN KEY(author_id) REFERENCES author(id),
 title VARCHAR(128) NOT NULL,
 content TEXT,
 created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
@@ -85,4 +86,61 @@ id          author_id   title       content      created_at
 7           3           Note A      Content A b  2018-09-03 20:14:04
 8           3           Note B      Content B b  2018-09-03 20:14:23
 9           3           Note C      Content C b  2018-09-03 20:14:38
+*/
+
+
+/********* Select all notes by an author's name *********/
+SELECT title, content FROM note, author
+WHERE note.author_id = author.id
+AND author.firstname = "Harry"
+AND author.lastname = "Potter";
+/*
+title       content    
+----------  -----------
+Note One    Content One
+Note Two    Content Two
+Note Three  Content Thr
+*/
+
+
+/********* Select author for a particular note by note ID *********/
+SELECT firstname, lastname FROM note, author
+WHERE note.id = 9
+AND note.author_id = author.id;
+/*
+firstname   lastname  
+----------  ----------
+Hermione    Granger  
+*/
+
+
+/********* Select the names of all the authors along with the number of notes they each have ********/
+SELECT firstname, lastname, COUNT(note.id)
+FROM note, author
+WHERE author.id = note.author_id
+GROUP BY author.id;
+/*
+firstname   lastname    COUNT(note.id)
+----------  ----------  --------------
+Harry       Potter      3             
+Ron         Weasley     3             
+Hermione    Granger     3 
+*/
+
+
+/********* Delete authors from the author table *********/
+DELETE FROM author
+WHERE author.id = 3;
+/*
+Error: FOREIGN KEY constraint failed
+
+What happens when you try to delete an author with an existing note?
+    Foreign key constraint don't allow the data that have the relationship with other tables delete or edit.
+
+How can you prevent this?
+    Add `ON DELETE CASCADE` clause to the foreign key for example 
+    `FOREIGN KEY(author_id) REFERENCES author(id) ON DELETE CASCADE`.
+
+    refer to https://www.sqlite.org/foreignkeys.html 
+    A "CASCADE" action propagates the delete or update operation on the parent key to each dependent child key. For an "ON DELETE CASCADE" action, this means that each row in the child table that was associated with the deleted parent row is also deleted. For an "ON UPDATE CASCADE" action, it means that the values stored in each dependent child key are modified to match the new parent key values.
 */
