ee1ae12b5ffd39bb8ee74c6eb47d1f39691dd4c5
Danny Vail <danny_vail@outlook.com>
2018-09-04 17:19:16 -0400

    SAVE COMMANDS FOR CREATING TRACK AND ARTIST_ALBUM TABLE In music.sql - Save command to create track table - Save command to create artist_album table

---
+++ music.sql
@@ -0,0 +1,15 @@
+-- CREATE TABLE [table name] ([column name][column data type][column options])
+
+-- Create a table called track
+CREATE TABLE track (
+  id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  title VARCHAR(128) NOT NULL,
+  album_id INT NOT NULL REFERENCES album(id)
+);
+
+-- Create a table called artist_album
+CREATE TABLE artist_album (
+  id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  artist_id INT NOT NULL REFERENCES artist(id),
+  album_id INT NOT NULL REFERENCES album(id)
+);
