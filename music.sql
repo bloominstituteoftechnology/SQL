bacbce0a697577385933b77b79f12b6e64ba16ea
udykas <udykas@gmail.com>
2018-09-03 21:38:46 -0400

    First set of queries finished

--- music.sql
+++ music.sql
@@ -8,7 +8,7 @@ PRAGMA foreign_keys = ON
 .mode COLUMN
 .header ON
 
-/*Create Tables*/
+/*CREATE TABLES*/
 
 CREATE TABLE album (
     id INTEGER PRIMARY KEY AUTOINCREMENT, 
@@ -31,3 +31,36 @@ CREATE TABLE artist_album (
     artist_id INT REFERENCES artist(id),
     album_id INT REFERENCES album(id)
 );
+
+/*POPULATE TABLES*/
+
+.read setup.sql
+
+/*ALBUM QUERIES AND OUTPUT*/
+
+/*Show all albums*/
+SELECT * FROM album;
+
+-- 1|Super Awesome Album|1990
+-- 2|Super Funky Album|
+-- 3|Super Disco Album|1978
+-- 4|Super Hairband Album|1984
+-- 5|Super Dubstep Album|
+
+/*Show all albums between 1975 and 1990*/
+SELECT * from album WHERE 1975 < release_year AND release_year < 1990;
+
+-- 3|Super Disco Album|1978
+-- 4|Super Hairband Album|1984
+
+/*Show all albums that start with "Super D" */
+SELECT * from album WHERE title LIKE 'Super D%';
+
+-- 3|Super Disco Album|1978
+-- 5|Super Dubstep Album|
+
+/*Show all albums with no release year*/
+SELECT * from album WHERE release_year IS NULL;
+
+-- 2|Super Funky Album|
+-- 5|Super Dubstep Album|
