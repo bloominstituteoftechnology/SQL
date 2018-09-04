46e45490ca51fb9f56c2bb305364dadb40457649
Sam-Park <sam.park.email@gmail.com>
2018-09-04 17:59:15 -0400

    day 1 complete

--- music.sql
+++ music.sql
@@ -26,4 +26,20 @@ SELECT * FROM album;
 SELECT * FROM album WHERE release_year > 1974 AND release_year < 1991;
 SELECT * FROM album WHERE title LIKE '%Super D%';
 SELECT * from album WHERE release_year IS NULL;
-SELECT track.title FROM track, album WHERE track.album_id = album.id AND album.title = 'Super Funky Album';
+
+
+SELECT album.title FROM album, artist, artist_album WHERE artist.id = artist_album.artist_id 
+AND album.id = artist_album.album_id AND artist.name = 'Han Solo';
+
+SELECT track.title FROM track, album 
+WHERE track.album_id = album.id AND album.title = 'Super Funky Album';
+
+SELECT AVG(release_year) FROM album, artist, artist_album WHERE artist.id = artist_album.artist_id 
+AND album.id = artist_album.album_id AND artist.name = 'Leia and the Ewoks';
+
+SELECT AVG(release_year) from album;
+
+SELECT COUNT(track.id) FROM track, album 
+WHERE album.id = track.album_id and album.title = 'Super Dubstep Album';
+
+SELECT COUNT(id) FROM artist;
