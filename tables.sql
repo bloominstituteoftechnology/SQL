b4f62c443c31268354923dacc0086fe99de0b1eb
Zach Campbell <beazlebubbles@gmail.com>
2018-09-04 13:59:53 -0600

    fixed and populated tables

--- tables.sql
+++ tables.sql
@@ -1,11 +1,22 @@
+CREATE TABLE album (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title VARCHAR(128) NOT NULL,
+    release_year INTEGER
+);
+
+CREATE TABLE artist (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(128) NOT NULL
+);
+
 CREATE TABLE track (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     title VARCHAR(128) NOT NULL,
-    albumid INT REFERENCES album(id)
+    album_id INT REFERENCES album(id)
 );
 
 CREATE TABLE artist_album (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    albumid INT REFERENCES album(id),
-    artistid INT REFERENCES artist(id)
+    album_id INT REFERENCES album(id),
+    artist_id INT REFERENCES artist(id)
 );
