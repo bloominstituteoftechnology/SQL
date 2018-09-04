3b4b952acca6ff7908f0eeaa92ecc4b7280a44dd
Zach Campbell <beazlebubbles@gmail.com>
2018-09-04 14:54:24 -0600

    Completed day 1 queries

--- queries.sql
+++ queries.sql
@@ -10,3 +10,29 @@ SELECT *
 SELECT *
     FROM album
     WHERE album.release_year IS NULL;
+
+SELECT track.title
+    FROM track, album
+    WHERE album.id = track.album_id AND album.title = 'Super Funky Album';
+
+SELECT track.title AS Track_Title
+    FROM track, album
+    WHERE album.id = track.album_id AND album.title = 'Super Funky Album';
+
+SELECT album.title
+    FROM album, artist, artist_album
+    WHERE artist_album.album_id = album.id AND artist_album.artist_id = artist.id AND artist.name = 'Han Solo';
+
+SELECT AVG(release_year)
+    FROM album;
+
+SELECT AVG(album.release_year)
+    FROM album, artist, artist_album
+    WHERE artist_album.album_id = album.id AND artist_album.artist_id = artist.id AND artist.name = 'Leia and the Ewoks';
+
+SELECT COUNT(*)
+    FROM artist;
+
+SELECT COUNT(track.id)
+    FROM track, album
+    WHERE album.id = track.album_id AND album.title = 'Super Dubstep Album';
