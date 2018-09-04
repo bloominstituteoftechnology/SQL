b521858d20a90ac1a748156c3604bde5ebd1e632
Brandon Hopper <brandonhopper0@gmail.com>
2018-09-04 18:02:39 -0400

    wrote queries for note.sql

---
+++ note.sql
@@ -0,0 +1,12 @@
+INSERT INTO author (name) VALUES ('Brandon Hopper');
+INSERT INTO author (name) VALUES ('Dj Hopper');
+INSERT INTO author (name) VALUES ('Bailey Hopper');
+
+INSERT INTO note (content, author_id) VALUES ('This is a note with some content to it', 2);
+INSERT INTO note (content, author_id) VALUES ('This is note is new and it is with some new content to it', 2);
+
+INSERT INTO note (content, author_id) VALUES ('This is some note with some content to it', 3);
+
+INSERT INTO note (content, author_id) VALUES ('This is one note with some content to it', 1);
+INSERT INTO note (content, author_id) VALUES ('This is a new note with some content to it', 1);
+INSERT INTO note (content, author_id) VALUES ('This is a another note with some content to it', 1);
